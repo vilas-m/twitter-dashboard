812a76363b5ba33eabb60e376c6e0b2a24bafb52
vilas-m <vilas@humancloud.co.in>
2021-04-29 17:19:01 +0530

    Added containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Main from "./Containers/Main";
 
-      function App() {
-          return (
-              <div>
-              Welcome
-              </div>
-          );}
-          
-      export default App;
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="left-half">
+        <div className="l-content-1"></div>
+        <div className="l-content-2"></div>
+        <div className="l-content-3"></div>
+        <div className="l-content-4"></div>
+      </div>
+      <div className="right-half"></div>
+    </div>
+  );
+};
+
+export default Main;
