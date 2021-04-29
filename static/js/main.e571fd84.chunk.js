ee4fd4e40a427ab77fd713e650298f06948cc837
vilas-m <vilas@humancloud.co.in>
2021-04-29 11:52:06 +0530

    Updates

---
+++ static/js/main.e571fd84.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdashboard2=this.webpackJsonpdashboard2||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n.n(c),r=n(3),i=n.n(r),s=n(0);var a=function(){return Object(s.jsx)("div",{children:"Welcome"})},d=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(t){var n=t.getCLS,c=t.getFID,o=t.getFCP,r=t.getLCP,i=t.getTTFB;n(e),c(e),o(e),r(e),i(e)}))};i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root")),d()}},[[9,1,2]]]);
+//# sourceMappingURL=main.e571fd84.chunk.js.map
